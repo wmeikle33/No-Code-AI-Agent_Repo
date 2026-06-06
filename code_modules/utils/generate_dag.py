019110dacadd1c2151d8953b0daf89fb6f0f7ead
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-06-06 21:49:18 +0800

    Update generate_dag.py

--- code_modules/utils/generate_dag.py
+++ code_modules/utils/generate_dag.py
@@ -1,1 +1,16 @@
-
+def create_dag():
+    dag = DAG(
+        dag_id=dag_id,
+        schedule=schedule,
+        default_args=default_args,
+        catchup=False
+    )
+    
+    with dag:
+        # Define tasks within the function
+        task1 = BashOperator(
+            task_id='echo_something',
+            bash_command='echo "Running DAG: {{ dag.dag_id }}"'
+        )
+        
+    return dag
