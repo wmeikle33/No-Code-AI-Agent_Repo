814a81d5f25468d33440ab7623cabeee45383c96
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-06-07 21:21:29 +0800

    Update teams_actions.py

--- code_modules/connectors/teams/teams_actions.py
+++ code_modules/connectors/teams/teams_actions.py
@@ -1,1 +1,10 @@
+import pymsteams
 
+# Initialize the connector with your Teams Webhook URL
+teams_msg = pymsteams.connectorcard("<YOUR_MICROSOFT_WEBHOOK_URL>")
+
+# Add message payload
+teams_msg.text("Hello Team! This automated alert was sent from a Python script.")
+
+# Execute the POST request
+teams_msg.send()
