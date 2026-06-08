32086b03e61aee9bb4bb91604f913ae2ca3227fe
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-06-09 00:11:59 +0800

    Create extract_key_feedback_themes.py

---
+++ code_modules/research/synthesis/extract_key_feedback_themes.py
@@ -0,0 +1,6 @@
+def extract_key_feedback_themes(department_name):
+    return {
+        "department_name": department_name,
+        "status": "requires_department_survey_data",
+        "themes": []
+    }
