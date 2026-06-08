8b9478d81a6601d04489075040d0904d230651f3
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-06-09 00:03:53 +0800

    Create analyze_talent_scarcity.py

---
+++ code_modules/research/discovery/analyze_talent_scarcity.py
@@ -0,0 +1,7 @@
+def analyze_talent_scarcity(skill_keywords, location):
+    return {
+        "skills": skill_keywords,
+        "location": location,
+        "status": "requires_job_market_data",
+        "scarcity_score": None
+    }
