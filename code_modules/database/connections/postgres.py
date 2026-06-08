052bdb85405ce210580af5748a02413a39929ee5
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-06-08 11:20:39 +0800

    Update postgres.py

--- code_modules/database/connections/postgres.py
+++ code_modules/database/connections/postgres.py
@@ -1,1 +1,13 @@
-
+def read_database(condition):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            host="localhost",
+            database="your_database",
+            user="your_username",
+            password="your_password",
+            port="5432"
+        )
+      
+    except (Exception, Error) as error:
+        print("Error while fetching data from PostgreSQL:", error)
