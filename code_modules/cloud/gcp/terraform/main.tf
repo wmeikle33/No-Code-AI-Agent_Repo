51b6214e437b2f236af8ad35609770bc842f85e2
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-06-09 23:42:41 +0800

    Update main.tf

--- code_modules/cloud/gcp/terraform/main.tf
+++ code_modules/cloud/gcp/terraform/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/gcp"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "gcp" {
+  region = "us-east-1"
+}
+
+resource "gcp_bucket" "example" {
+  bucket = "my-example-bucket-12345"
+}
